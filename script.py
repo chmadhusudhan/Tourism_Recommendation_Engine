2fb3cb6c1a1c0682df94b606b7478691e1f88045
Madhu Sudhan <chmadhusudhan97@gmail.com>
2020-07-23 17:08:52 +0530

    Added function to generate message for traveler and present attractions they might be interested in.

--- script.py
+++ script.py
@@ -1,6 +1,5 @@
 destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
 
-#Name, Where he is, what he likes
 test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
 
 def get_destination_index(destination):
@@ -18,7 +17,7 @@ test_destination_index = get_traveler_location(test_traveler)
 #Visiting Interesting Places
 
 attractions = [[], [], [], [], []]
-
+#print(attractions)
 
 def add_attraction(destination, attraction):
   try:
@@ -37,9 +36,46 @@ add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
 add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
 add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
 add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
-
-#print(attractions)
-#Finding the Best Places to Go
-
+ 
   
+#Finding the Best Places to Go
+def find_attractions(destination, interests):
+  destination_index = get_destination_index(destination)
+  attractions_in_city = attractions[destination_index]
+  attractions_with_interest = []
+  for attr_in_city in attractions_in_city:
+    possible_attraction = attr_in_city
+    attraction_tags = attr_in_city[1]
+    for interest in interests:
+      if interest in attraction_tags:
+        attractions_with_interest.append(possible_attraction[0])
+  return(attractions_with_interest)
+la_arts = find_attractions("Los Angeles, USA", ['art'])
+print(la_arts)
+
+#See The Parts of a City You want to See
+def get_attractions_for_traveler(traveler):
+  traveler_destination = traveler[1]
+  traveler_interests = traveler[2]
+  traveler_attractions = find_attractions(traveler_destination,traveler_interests)
+  interests_string = "Hi " + traveler[0] +", we think you'll like these places around " + traveler_destination + ": "
+  for attraction in traveler_attractions:
+    interests_string += attraction
+  return interests_string
+
+smills_france = get_attractions_for_traveler(['Dereck Smill', 'Paris, France', ['monument']])
+
+print(smills_france)
+
+
+
+
+
+
+
+
+
+    
+
+
 
